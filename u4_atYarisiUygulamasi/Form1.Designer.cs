fb43fa38a94081cf05efcad9a9e4021be1cde7a1
yasemin göktaş <ygkts61@gmail.com>
2019-01-24 23:22:52 +0300

    aaa

---
+++ u4_atYarisiUygulamasi/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace u4_atYarisiUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.pcbAt1 = new System.Windows.Forms.PictureBox();
+            this.pcbAt2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbAt1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbAt2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button1.Location = new System.Drawing.Point(51, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BASLAT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pcbAt1
+            // 
+            this.pcbAt1.BackColor = System.Drawing.Color.Transparent;
+            this.pcbAt1.Image = ((System.Drawing.Image)(resources.GetObject("pcbAt1.Image")));
+            this.pcbAt1.Location = new System.Drawing.Point(51, 440);
+            this.pcbAt1.Name = "pcbAt1";
+            this.pcbAt1.Size = new System.Drawing.Size(129, 106);
+            this.pcbAt1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbAt1.TabIndex = 1;
+            this.pcbAt1.TabStop = false;
+            // 
+            // pcbAt2
+            // 
+            this.pcbAt2.BackColor = System.Drawing.Color.Transparent;
+            this.pcbAt2.Image = ((System.Drawing.Image)(resources.GetObject("pcbAt2.Image")));
+            this.pcbAt2.Location = new System.Drawing.Point(51, 578);
+            this.pcbAt2.Name = "pcbAt2";
+            this.pcbAt2.Size = new System.Drawing.Size(129, 106);
+            this.pcbAt2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbAt2.TabIndex = 2;
+            this.pcbAt2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightGreen;
+            this.label1.Location = new System.Drawing.Point(48, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(897, 83);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "AT YARIŞINA HOŞGELDİNİZ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.CadetBlue;
+            this.label2.Location = new System.Drawing.Point(1006, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.CadetBlue;
+            this.label3.Location = new System.Drawing.Point(1006, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1208, 722);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pcbAt1);
+            this.Controls.Add(this.pcbAt2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbAt1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbAt2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pcbAt1;
+        private System.Windows.Forms.PictureBox pcbAt2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ u4_atYarisiUygulamasi/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace u4_atYarisiUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        int skorAt3 = 0;
+        int skorAt4 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 100;
+            timer1.Start();     //  veya =  timer1.Enabled = true;      
+            label1.Text = "YARIŞ BAŞLADI.";
+            pcbAt1.Left = 0;
+            pcbAt2.Left = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;       // program çalışırken form tam ekran açılsın
+            // butonun otomatik boyutlandırılması.
+            button1.AutoSizeMode = AutoSizeMode.GrowOnly;
+            button1.AutoSize = true;        // otomatik boyutlandırma aktif
+            button1.Dock = DockStyle.Top;   // yatayda sabitle
+            //label1.Dock = DockStyle.Top;
+
+            pcbAt1.Hide();
+            pcbAt2.Hide();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pcbAt1.Show();
+            pcbAt2.Show();
+            Random rnd = new Random();
+            int sayi1 = rnd.Next(10, 50);
+            int sayi2 = rnd.Next(10, 50);
+            
+            pcbAt1.Left += sayi1;   // pictureBox'ın(atın) sola olan uzaklığını sayi1 kadar arttır. Bu sayede at, random uzaklıklarla hareket ediyor.
+            pcbAt2.Left += sayi2;
+
+            if (pcbAt1.Right >= this.Width || pcbAt2.Right >= this.Width)     // atlardan herhangi birinin sağa olan yakınlığı, form2un genişliğini geçtiyse
+            {
+                timer1.Stop();  // veya = timer1.Enabled = false;  --> timer durdur.
+                label1.Text = "YARIŞ BİTTİ.";
+                if (pcbAt1.Right > pcbAt2.Right)
+                {
+                    MessageBox.Show("3 numaralı at KAZANDI...");
+                    skorAt3++;
+                    label2.Text = "3 Nolu At:  "+ skorAt3;
+                    
+                }
+                else
+                {
+                    MessageBox.Show("4 numaralı at KAZANDI...");
+                    skorAt4++;
+                    label3.Text = "4 Nolu At:  " + skorAt4;
+                    
+                }
+            }
+        }
+    }
+}
